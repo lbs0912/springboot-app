2858d15d2c56be41e34302e343c3f466c59becf5
liubaoshuai <liubaoshuai1@jd.com>
2020-06-10 15:11:25 +0800

    常规参数校验

--- src/main/java/com/lbs0912/springboot/app/entity/User.java
+++ src/main/java/com/lbs0912/springboot/app/entity/User.java
@@ -3,6 +3,10 @@ package com.lbs0912.springboot.app.entity;
 
 import com.lbs0912.springboot.app.common.BaseCommon;
 
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,21 +23,19 @@ public class User extends BaseCommon {
 
 //    @TableId(type = IdType.AUTO)
     private Long id;
-//
-//    @NotNull(message = "用户账号不能为空")
-//    @Size(min = 6, max = 11, message = "账号长度必须是6-11个字符")
+
+    @NotNull(message = "用户账号不能为空")
+    @Size(min = 6, max = 11, message = "账号长度必须是6-11个字符")
     private String account;
-//
-//    @NotNull(message = "用户密码不能为空")
-//    @Size(min = 6, max = 11, message = "密码长度必须是6-16个字符")
+
+    @NotNull(message = "用户密码不能为空")
+    @Size(min = 6, max = 11, message = "密码长度必须是6-16个字符")
     private String password;
-//
-//    @Size(max = 40, message = "用户昵称不能超过40个字符")
+
+    @Size(max = 40, message = "用户昵称不能超过40个字符")
     private String nickname;
-//
-//    @Email(message = "邮箱格式不正确")
+
     private String email;
 
-//    @Phone(message = "手机号格式不正确")
     private String phone;
 }
