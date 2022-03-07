69e35bbf79ef7526f11f4320d2aed5e3a747a2ca
lbs0912 <lbs1203940926@163.com>
2022-03-07 08:23:43 +0800

    fix

--- src/main/java/com/lbs0912/springboot/app/SpringbootAppApplication.java
+++ src/main/java/com/lbs0912/springboot/app/SpringbootAppApplication.java
@@ -5,11 +5,7 @@ import com.lbs0912.springboot.app.service.HelloWorld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.sql.SQLOutput;
-import java.util.Map;
 
 /**
  *
@@ -28,7 +24,7 @@ public class SpringbootAppApplication {
 		SpringApplication.run(SpringbootAppApplication.class, args);
 
 		//使用框架 API ClassPathXmlApplicationContext() 来创建应用程序的上下文。这个 API 加载 beans 的配置文件并最终基于所提供的 API，它处理创建并初始化所有的对象，即在配置文件中提到的 beans
-		ApplicationContext context = new ClassPathXmlApplicationContext("BeanConfig.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("beanConfig/BeanConfig.xml");
 		getMsgFromBean(context);
 
 
